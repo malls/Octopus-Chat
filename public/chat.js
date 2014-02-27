7fa85cfa84ed23c19df3eace37b188080c3e215b
Forrest Almasi <forrestalmasi@gmail.com>
2014-02-27 11:19:50 -0600

    attempt to fix port issue

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://192.168.1.4:3005');
+    var socket = io.connect('http://hidden-beach-6020.herokuapp.com');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
